abb43f896a9680743dd1876adc744b53c9d78d21
Agnieszka S <mail.sadowska@gmail.com>
2023-01-14 09:59:03 +0100

    Podstawowa wersja aplikacji Spring Boot

---
+++ src/main/java/edu/wsb/finalprojectlocal/FinalProjectLocalApplication.java
@@ -0,0 +1,13 @@
+package edu.wsb.finalprojectlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/wsb/finalprojectlocal/FinalProjectLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wsb.finalprojectlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
